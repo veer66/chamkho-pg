79d99d85e59754de1ecefe5e428128a41261764b
Vee Satayamas <vee.sa@protonmail.com>
2020-06-08 01:24:23 +0700

    get args

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,6 @@ use std::os::raw::c_char;
 
 pg_magic!(version: pg_sys::PG_VERSION_NUM);
 
-#[repr(C)]
-struct ChamkhoParser {
-    i: i32,
-}
-
 #[no_mangle]
 pub extern "C" fn pg_finfo_chamkho_parser_start() -> &'static pg_sys::Pg_finfo_record {
     &pg_extend::pg_sys::Pg_finfo_record { api_version: 1 }
@@ -37,27 +32,52 @@ pub extern "C" fn pg_finfo_chamkho_parser_lextype() -> &'static pg_sys::Pg_finfo
     &pg_extend::pg_sys::Pg_finfo_record { api_version: 1 }
 }
 
+#[repr(C)]
+struct ParserCtx {
+    i: i32,
+    // char* text;         // the current position of text
+    // int text_len;       // the length of text
+    // char* buf;          // A text to parse
+    // int buf_len;        // the length of text
+    // int *pos;           // The position of a breaking word
+    // int num;            // The number of a breaking word
+    // int cur_id;         // An ID of current word
+}
+
+#[repr(C)]
+struct LexDescr {
+    lexid: i32,
+    // int  lexid;
+    // char *alias;
+    // char *descr;
+}
+
 #[no_mangle]
-pub extern "C" fn chamkho_parser_start(_func_call_info: pg_sys::FunctionCallInfo) -> pg_sys::Datum {
-    info!("@@@@@@@1");
-    let x = unsafe { pg_sys::palloc(10) as *mut u8 };
-    //Box::into_raw(Box::new(ChamkhoParser { i: 10 })) as pg_sys::Datum
-    x as pg_sys::Datum
+pub extern "C" fn chamkho_parser_start(func_call_info: pg_sys::FunctionCallInfo) -> pg_sys::Datum {
+    let ctx = unsafe { pg_sys::palloc0(std::mem::size_of::<ParserCtx>() as u64) as pg_sys::Datum };
+    let args: Vec<_> = unsafe { pg_extend::get_args(func_call_info.as_mut().unwrap()) }.collect();
+    let text_len = args[1].unwrap() as i32;
+    info!("!!!! TEXT-LEN = {:?}", text_len);
+
+    let raw_text = args[0].unwrap() as *mut u8;
+    let text_u8 = unsafe { std::slice::from_raw_parts(raw_text, text_len as usize) };
+    let text = String::from_utf8(text_u8.to_vec()).unwrap();
+    info!("!!! TEXT = {}", &text);
+    ctx
 }
 
 #[no_mangle]
 pub extern "C" fn chamkho_parser_get_token(
-    _func_call_info: pg_sys::FunctionCallInfo,
+    func_call_info: pg_sys::FunctionCallInfo,
 ) -> pg_sys::Datum {
-    info!("@@@@@@@2");
-    let x = unsafe { pg_sys::palloc(10) as *mut u8 };
+    let ctx = unsafe { pg_sys::palloc0(std::mem::size_of::<ParserCtx>() as u64) as pg_sys::Datum };
+//    let text = unsafe { pg_sys::PG_GETARG_POINTER(0) };
     //Box::into_raw(Box::new(ChamkhoParser { i: 10 })) as pg_sys::Datum
-    x as pg_sys::Datum
+    ctx
 }
 
 #[no_mangle]
 pub extern "C" fn chamkho_parser_end(_func_call_info: pg_sys::FunctionCallInfo) -> pg_sys::Datum {
-    info!("@@@@@@@3");
     let x = unsafe { pg_sys::palloc(10) as *mut u8 };
     //Box::into_raw(Box::new(ChamkhoParser { i: 10 })) as pg_sys::Datum
     x as pg_sys::Datum
@@ -67,7 +87,6 @@ pub extern "C" fn chamkho_parser_end(_func_call_info: pg_sys::FunctionCallInfo) 
 pub extern "C" fn chamkho_parser_lextype(
     _func_call_info: pg_sys::FunctionCallInfo,
 ) -> pg_sys::Datum {
-    info!("@@@@@@@4");
     let x = unsafe { pg_sys::palloc(10) as *mut u8 };
     //Box::into_raw(Box::new(ChamkhoParser { i: 10 })) as pg_sys::Datum
     x as pg_sys::Datum
