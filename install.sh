7853b927034e72b1ab142b79b462c7d3ee894d82
Prathan Thananart <prathan@hey.com>
2024-12-09 23:51:14 +0700

    Rebase to build against 16.6 both in Dockerfile and install script

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# Determine the platform-specific library extension
-if [ "$(uname)" = "Darwin" ]; then
-    LIB_EXT="dylib"
-else
-    LIB_EXT="so"
-fi
-
 # Copy the library with the correct extension
-cp "target/release/libchamkho_parser.$LIB_EXT" "$(pg_config --libdir)/postgresql/chamkho_parser.so"
+if [ "$(uname)" = "Darwin" ]; then
+    # Depending on the brew formula version, the expected extension can be either .dylib or .so
+    cp "target/release/libchamkho_parser.dylib" "$(pg_config --libdir)/postgresql/chamkho_parser.dylib"
+    ln -sf "$(pg_config --libdir)/postgresql/chamkho_parser.dylib" "$(pg_config --libdir)/postgresql/chamkho_parser.so"
+else
+    cp "target/release/libchamkho_parser.so" "$(pg_config --libdir)/postgresql/chamkho_parser.so"
+fi
 
 # Copy extension files
 cp control/*.control sql/*.sql "$(pg_config --sharedir)/extension"
